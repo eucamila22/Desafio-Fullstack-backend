e74c44374fc544c29a3edad04f74494fb484cfe0
Camila Gonçalves Domingos Tavares <105559819+eucamila22@users.noreply.github.com>
2023-05-28 18:03:44 -0300

    fix/making the relationship between customers and contacts appear in the route listAllContacts

--- src/services/contact/listAllContacts.service.ts
+++ src/services/contact/listAllContacts.service.ts
@@ -1,14 +1,31 @@
-import { AppDataSource } from '../../data-source'
-import { Contact } from '../../entities/contact.entity'
-import { iContactRepo, iContactsReturn } from '../../interfaces/contact.interface'
-import { returnMultipleContactSchema } from '../../schemas/contact.schema'
+// import { AppDataSource } from '../../data-source'
+// import { Contact } from '../../entities/contact.entity'
+// import { iContactRepo, iContactsReturn } from '../../interfaces/contact.interface'
+// import { returnMultipleContactSchema } from '../../schemas/contact.schema'
 
-const listAllContactService = async (): Promise<iContactsReturn> => {
-    const contactRepository: iContactRepo = AppDataSource.getRepository(Contact)
-    const findContacts: Array<Contact> = await contactRepository.find()
-    const contacts = returnMultipleContactSchema.parse(findContacts)
+// const listAllContactService = async (): Promise<iContactsReturn> => {
+//     const contactRepository: iContactRepo = AppDataSource.getRepository(Contact)
+//     const findContacts: Array<Contact> = await contactRepository.find()
+//     const contacts = returnMultipleContactSchema.parse(findContacts)
 
-    return contacts
-}
+//     return contacts
+// }
 
-export default listAllContactService
+// export default listAllContactService
+
+import { AppDataSource } from '../../data-source';
+import { Contact } from '../../entities/contact.entity';
+import { iContact, iContactRepo } from '../../interfaces/contact.interface';
+
+const listAllContactService = async (): Promise<iContact[]> => {
+  const contactRepository: iContactRepo = AppDataSource.getRepository(Contact);
+  const contacts: iContact[] = await contactRepository.find({
+    relations: {
+      client: true,
+    },
+  });
+
+  return contacts;
+};
+
+export default listAllContactService;
