68772b00f3c968fe16dd0030670e4f5ed933919f
Camila Gonçalves Domingos Tavares <105559819+eucamila22@users.noreply.github.com>
2023-06-01 17:03:38 -0300

    feat/api documentation

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ import clientRoutes from './routers/client.routes'
 import cors from 'cors'
 import loginRoutes from './routers/login.routes'
 import contactRoutes from './routers/contact.routes'
-// import pdfReportRoutes from './routers/pdf.routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 
 const app: Application = express()
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
 app.use('/clients', clientRoutes)
 app.use('/login', loginRoutes)
 app.use('/contacts', contactRoutes)
-// app.use('/report', pdfReportRoutes)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 
 app.use(handleErrors)
 export default app
-
